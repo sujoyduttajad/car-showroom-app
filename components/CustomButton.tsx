bbe708c89a2def27eadf7dc417231950cccf7bba
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-06-12 21:43:17 +0530

    Update CustomButton.tsx

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -9,8 +9,8 @@ const CustomButton = ({
   return (
     <button
       disabled={false}
-      type={"button"}
-      className={`custom-btn`}
+      type="button"
+      className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
       <span className={`flex-1`}>{title}</span>
