cfdfea8105b033a204144cb499d7fdbf698b9555
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-06-12 15:49:00 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import { Hero } from "@/components";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-     
+      <Hero />
     </main>
-  )
+  );
 }
