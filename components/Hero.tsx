35e18c3a199e1433dbd4caee22771c24e110a48c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-06-12 15:59:40 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, or rent a car -- quickly and easily!
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
